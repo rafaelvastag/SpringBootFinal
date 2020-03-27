df6977f33d9c565e4a2c0a6200c0ae0e26e2a1ba
838146 <838146@LTBR97251.cts.com>
2020-03-27 13:26:32 -0300

    Inserindo novo CLIENTE + Enderecos + telefones

--- cursomc-master/src/main/java/com/rafaelvastag/springboot/services/ClienteService.java
+++ cursomc-master/src/main/java/com/rafaelvastag/springboot/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.rafaelvastag.springboot.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.rafaelvastag.springboot.domain.Cidade;
 import com.rafaelvastag.springboot.domain.Cliente;
+import com.rafaelvastag.springboot.domain.Endereco;
+import com.rafaelvastag.springboot.domain.enums.TipoCliente;
 import com.rafaelvastag.springboot.dto.ClienteDTO;
+import com.rafaelvastag.springboot.dto.ClienteNewDTO;
 import com.rafaelvastag.springboot.repositories.ClienteRepository;
+import com.rafaelvastag.springboot.repositories.EnderecoRepository;
 import com.rafaelvastag.springboot.services.exceptions.DataIntegrityException;
 import com.rafaelvastag.springboot.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +28,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -28,11 +38,19 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
-		
+
 	}
 
 	public void delete(Integer id) {
@@ -56,7 +74,26 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli =  new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), 
+							TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(),
+									objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2()!= null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3()!= null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
+		
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
